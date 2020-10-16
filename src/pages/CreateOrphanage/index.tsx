8192d506e17af53907ffb49f4428787eb17796b0
Sergio Pereira <sergiohgp@gmail.com>
2020-10-16 01:53:18 -0400

    Create Orphanages and Orphanages pages

---
+++ src/pages/CreateOrphanage/index.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import { useHistory } from 'react-router-dom';
+
+import { FiArrowLeft, FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../../assets/happyface.svg';
+
+import {
+  Container,
+  Main,
+  Aside,
+  Form,
+  InputBlock,
+  ButtonSelect,
+  ButtonConfirm,
+} from './styles';
+
+const happyMapIcon = L.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60],
+});
+
+export default function CreateOrphanage() {
+  const { goBack } = useHistory();
+
+  return (
+    <Container id="page-create-orphanage">
+      <Aside>
+        <img src={mapMarkerImg} alt="Happy" />
+
+        <footer>
+          <button type="button" onClick={goBack}>
+            <FiArrowLeft size={24} color="#FFF" />
+          </button>
+        </footer>
+      </Aside>
+
+      <Main>
+        <Form className="create-orphanage-form">
+          <fieldset>
+            <legend>Dados</legend>
+
+            <Map
+              center={[-27.2092052, -49.6401092]}
+              style={{ width: '100%', height: 280 }}
+              zoom={15}
+            >
+              <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+              />
+
+              <Marker
+                interactive={false}
+                icon={happyMapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
+            </Map>
+
+            <InputBlock className="input-block">
+              <label htmlFor="name">Nome</label>
+              <input id="name" />
+            </InputBlock>
+
+            <InputBlock className="input-block">
+              <label htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
+              <textarea id="name" maxLength={300} />
+            </InputBlock>
+
+            <InputBlock className="input-block">
+              <label htmlFor="images">Fotos</label>
+
+              <div className="uploaded-image" />
+
+              <button className="new-image">
+                <FiPlus size={24} color="#15b6d6" />
+              </button>
+            </InputBlock>
+          </fieldset>
+
+          <fieldset>
+            <legend>Visitação</legend>
+
+            <InputBlock className="input-block">
+              <label htmlFor="instructions">Instruções</label>
+              <textarea id="instructions" />
+            </InputBlock>
+
+            <InputBlock className="input-block">
+              <label htmlFor="opening_hours">Nome</label>
+              <input id="opening_hours" />
+            </InputBlock>
+
+            <InputBlock className="input-block">
+              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+
+              <ButtonSelect className="button-select">
+                <button type="button" className="active">
+                  Sim
+                </button>
+                <button type="button">Não</button>
+              </ButtonSelect>
+            </InputBlock>
+          </fieldset>
+
+          <ButtonConfirm className="confirm-button" type="submit">
+            Confirmar
+          </ButtonConfirm>
+        </Form>
+      </Main>
+    </Container>
+  );
+}
+
+// return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import { Container, Content, AnimationContainer } from './styles';
 
 import happyfaceImg from '../../assets/happyface.svg';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: happyfaceImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 10],
+});
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -28,7 +36,7 @@ const OrphanagesMap: React.FC = () => {
           </aside>
         </AnimationContainer>
       </Content>
-      <Link to="/app">
+      <Link to="/orphanages/create">
         <FiPlus size={26} color="rgba(0, 0, 0, 0.6)" />
       </Link>
 
@@ -44,6 +52,19 @@ const OrphanagesMap: React.FC = () => {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+        <Marker icon={mapIcon} position={[43.738299, -79.5047]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            AACC
+            <Link to="/orphanages/f6fab9fe-bb02-4f8a-a584-ee99bd1fa8f1">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
     </Container>
   );
